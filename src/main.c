81e54f6915b464f858d9aaa4875bec358e484670
Broccoliraab <broccoliraab>
2022-06-03 15:07:21 -0400

    Refactor collision

--- src/main.c
+++ src/main.c
@@ -137,15 +137,17 @@ void move_player(Camera *cam, i32f map[100], f64 movespeed, f64 rotspeed){
     f64 pos_y =0;
     const u8 *keyboard = SDL_GetKeyboardState(NULL);
     if(keyboard[SDL_SCANCODE_UP]){
-        if(tile_from_position(map, (i32f) cam->pos_x+cam->dir_x*movespeed, (i32f) cam->pos_y)<0)
-            pos_x = cam->dir_x*movespeed; 
-        if(tile_from_position(map, (i32f) cam->pos_x, (i32f) cam->pos_y+cam->dir_y*movespeed)<0)
-            pos_y = cam->dir_y*movespeed;
+        pos_x = cam->dir_x*movespeed; 
+        pos_y = cam->dir_y*movespeed;
     }else if(keyboard[SDL_SCANCODE_DOWN]){
-        if(tile_from_position(map, (i32f) cam->pos_x-cam->dir_x*movespeed, (i32f) cam->pos_y)<0)
-            pos_x = -cam->dir_x*movespeed;
-        if(tile_from_position(map, (i32f) cam->pos_x, (i32f) cam->pos_y-cam->dir_y*movespeed)<0)
-            pos_y = -cam->dir_y*movespeed;
+        pos_x = -cam->dir_x*movespeed;
+        pos_y = -cam->dir_y*movespeed;
+    }
+    if(tile_from_position(map, (i32f) cam->pos_x+pos_x, (i32f) cam->pos_y)>-1){
+        pos_x =0;
+    }
+    if(tile_from_position(map, (i32f) cam->pos_x, (i32f) cam->pos_y+pos_y)>-1){
+        pos_y =0;
     }
     if(keyboard[SDL_SCANCODE_RIGHT]) {
         f64 old_dir_x =cam->dir_x;
