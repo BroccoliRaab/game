18b9935493029f932636ae06a40aa64a46d94533
robert <>
2023-07-11 16:29:24 -0400

    initial texture mapping

--- makefile
+++ makefile
@@ -4,14 +4,13 @@ SRC=src/*
 GAME=game
 
 SDL_LIBS!=sdl2-config --cflags --libs
-LIBS=-lm $(SDL_LIBS) -lSDL2_ttf
-CFLAGS?=-std=c17 -Wall -pedantic -Werror
+LIBS=-lm $(SDL_LIBS) -lSDL2_ttf -lSDL2_image
+CFLAGS+=-O0 -g -std=c17 -Wall -pedantic 
 INCLUDE=-Iinclude
 
 $(GAME): $(SRC)
 	mkdir -p bin
-	$(CC) $(SRC) $(LIBS) -o bin/$(GAME) $(INCLUDE) $(CFLAGS)
-
+	$(CC) $(SRC) $(LIBS) $(CFLAGS) -o bin/$(GAME) $(INCLUDE) 
 compile_flags.txt:
 	echo "$(LIBS) $(INCLUDE)" | sed 's/ /\n/g' > compile_flags.txt
 
--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <math.h>
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <SDL_ttf.h>
 
 #include "gtypes.h"
@@ -23,8 +24,9 @@ typedef struct Camera {
 
 
 void move_player(Camera *cam, Tilemap *map, f64 movespeed, f64 rotspeed);
-void draw_walls(Camera *cam,SDL_Renderer *renderer, Tilemap *map);
+void draw_walls(Camera *cam,SDL_Renderer *renderer, Tilemap *map, SDL_Surface * tile_img);
 i32f tile_from_position(Tilemap *map, i32f pos_x, i32f pos_y);
+i32f DrawVTexLine(SDL_Renderer *renderer, SDL_Surface *tex, i32f scr_x, i32f line_start, i32f line_end, i32f tex_x);
 
 int main(void) {
     Tilemap map;
@@ -42,6 +44,14 @@ int main(void) {
         goto EXIT_main;
     }
     fclose(f);
+
+    SDL_Surface * tile_img = IMG_Load("assets/tileset/brks.bmp");
+    tile_img = SDL_ConvertSurfaceFormat(tile_img, SDL_PIXELFORMAT_ABGR8888, 0);
+    if (!tile_img){
+        fputs("Failed to open tile image\n", stderr);
+        main_ret = 1;
+        goto EXIT_main;
+    }
 
     i32f sdl_init_retval = SDL_Init(SDL_INIT_EVERYTHING);
     if (sdl_init_retval<0){
@@ -64,7 +74,8 @@ int main(void) {
         goto EXIT_main;
     }
 
-    SDL_Renderer * renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC);
+    SDL_Renderer * renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+    //|SDL_RENDERER_PRESENTVSYNC
     if(!renderer) {
         fputs("Failed to create renderer\n", stderr);
         main_ret = 1;
@@ -77,6 +88,8 @@ int main(void) {
         main_ret = 1;
         goto EXIT_main;
     }
+
+    SDL_Texture *tile_img_tex = SDL_CreateTextureFromSurface(renderer, tile_img);
 
     Camera main_camera;
     
@@ -109,7 +122,7 @@ int main(void) {
             }
         }
         move_player(&main_camera, &map, movespeed, rotspeed);
-        draw_walls(&main_camera, renderer, &map);
+        draw_walls(&main_camera, renderer, &map, tile_img);
 
         char fps_str[20];
         char pos_str[25];
@@ -124,9 +137,11 @@ int main(void) {
 
         SDL_Texture *position_display_texture = SDL_CreateTextureFromSurface(renderer, position_display);
         SDL_Rect position_rect = (SDL_Rect) {0, fps_counter->h+5, position_display->w, position_display->h};
+        SDL_Rect img_position_rect = (SDL_Rect) {0, position_rect.h+32, tile_img->w, tile_img->h};
 
         SDL_RenderCopy(renderer, fps_counter_texture, NULL, &counter_rect);
         SDL_RenderCopy(renderer, position_display_texture, NULL, &position_rect);
+        SDL_RenderCopy(renderer, tile_img_tex, NULL, &img_position_rect);
 
         SDL_RenderPresent(renderer);
         SDL_RenderClear(renderer);
@@ -140,6 +155,8 @@ int main(void) {
 EXIT_main:
 
     TTF_CloseFont(font);
+
+    SDL_FreeSurface(tile_img);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(win);
     TTF_Quit();
@@ -183,7 +200,7 @@ void move_player(Camera *cam, Tilemap *map, f64 movespeed, f64 rotspeed){
     cam->pos_x+= pos_x;
     cam->pos_y+= pos_y;
 }
-void draw_walls(Camera *cam,SDL_Renderer *renderer, Tilemap *map){
+void draw_walls(Camera *cam,SDL_Renderer *renderer, Tilemap *map, SDL_Surface* tile_img){
     for (i32f x =0; x<SCREEN_WIDTH; x++){
         f64 cam_x =2 * (f64)x/(f64)SCREEN_WIDTH-1;
         f64 ray_dir_x = cam->dir_x +cam->plane_x *cam_x;
@@ -239,20 +256,93 @@ void draw_walls(Camera *cam,SDL_Renderer *renderer, Tilemap *map){
         }else {
             wall_dist = (side_dist_y -delta_dist_y);
         }
+    
         i32f lineheight  = (i32f)(SCREEN_HEIGHT /wall_dist);
 
         i32f line_start = -lineheight /2 +SCREEN_HEIGHT/2;
         if (line_start <0) line_start =0;
         i32f line_end = lineheight /2 + SCREEN_HEIGHT/2;
         if(line_end >=SCREEN_HEIGHT) line_end = SCREEN_HEIGHT-1;
+        /*
         SDL_SetRenderDrawColor(renderer, (side?100:200),0,0,255);
         i32f drawline_retval = SDL_RenderDrawLine(renderer, x, line_start, x, line_end);
-        if(!drawline_retval)
-            fprintf(stderr, "WARN: Failed to draw line in column %d\n", x);
+        if(drawline_retval)
+            fprintf(stderr, "WARN: Failed to draw line in column %d.\n%s\n", x, SDL_GetError());
+        */
+        f64 wall_x;
+        
+        //calculate value of wallX
+        if (side == 0) wall_x = cam->pos_y + wall_dist * ray_dir_y;
+        else           wall_x = cam->pos_x + wall_dist * ray_dir_x;
+        wall_x -= floor((wall_x));
+
+        //x coordinate on the texture
+        i32f tex_x = (wall_x * tile_img->w );
+        if(side == 0 && ray_dir_x > 0) tex_x = tile_img->w - tex_x - 1;
+        if(side == 1 && ray_dir_y < 0) tex_x = tile_img->w - tex_x - 1;
+
+        DrawVTexLine(renderer,
+            tile_img,
+            x,
+            line_start,
+            line_end,
+            tex_x);
+
         SDL_SetRenderDrawColor(renderer, 0,0,0,255);
     }
 }
 
+i32f DrawVTexLine(SDL_Renderer *renderer, SDL_Surface *tex, i32f scr_x, i32f line_start, i32f line_end, i32f tex_x){
+    SDL_PixelFormat *fmt = tex->format;
+    SDL_Surface *surface = tex;
+    Uint32 temp, pixel;
+    Uint8 red, green, blue, alpha;
+    Uint32 tex_y;
+    f64 tex_y_f;
+    Uint32 *pixel_buf = (tex->pixels);
+    for (int i=0; i<line_end-line_start ; i++)
+    {
+
+        fmt = surface->format;
+        SDL_LockSurface(surface);
+        tex_y_f = (((f64)i)/(f64)(line_end-line_start)) * (f64)surface->h;
+        tex_y = (Uint32) floor(tex_y_f);
+        pixel = pixel_buf[tex_x+tex_y*tex->h];
+
+        
+        SDL_UnlockSurface(surface);
+
+        /* Get Red component */
+        temp = pixel & fmt->Rmask;  /* Isolate red component */
+        temp = temp >> fmt->Rshift; /* Shift it down to 8-bit */
+        temp = temp << fmt->Rloss;  /* Expand to a full 8-bit number */
+        red = (Uint8)temp;
+
+        /* Get Green component */
+        temp = pixel & fmt->Gmask;  /* Isolate green component */
+        temp = temp >> fmt->Gshift; /* Shift it down to 8-bit */
+        temp = temp << fmt->Gloss;  /* Expand to a full 8-bit number */
+        green = (Uint8)temp;
+
+        /* Get Blue component */
+        temp = pixel & fmt->Bmask;  /* Isolate blue component */
+        temp = temp >> fmt->Bshift; /* Shift it down to 8-bit */
+        temp = temp << fmt->Bloss;  /* Expand to a full 8-bit number */
+        blue = (Uint8)temp;
+
+        /* Get Alpha component */
+        temp = pixel & fmt->Amask;  /* Isolate alpha component */
+        temp = temp >> fmt->Ashift; /* Shift it down to 8-bit */
+        temp = temp << fmt->Aloss;  /* Expand to a full 8-bit number */
+        alpha = (Uint8)temp;
+
+
+        SDL_SetRenderDrawColor(renderer, red, green, blue, alpha);
+        SDL_RenderDrawPoint(renderer, scr_x, line_start+i);
+    }
+    return 0;
+
+}
 
 
 i32f tile_from_position(Tilemap *map,i32f pos_x, i32f pos_y){
